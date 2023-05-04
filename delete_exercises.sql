fc871cfa7f1c2924fcbd0d88a6cb6202fb31caf3
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-05-04 14:07:57 -0500

    db connected

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,13 +1,13 @@
 USE codeup_test_db;
 
-SELECT * FROM albums WHERE date > 1991;
+SELECT * FROM albums WHERE release_date > 1991;
 
-SELECT * FROM albums WHERE genre = 'Disco';
-
+# SELECT * FROM albums WHERE genre = 'Disco';
+#
 SELECT * FROM albums WHERE artist = 'The Beatles';
-
-DELETE FROM albums WHERE date > 1991;
-
-DELETE FROM albums WHERE genre = 'Disco';
-
-DELETE FROM albums WHERE artist = 'The Beatles';
+#
+# DELETE FROM albums WHERE release_date > 1991;
+#
+# DELETE FROM albums WHERE genre = 'Disco';
+#
+# DELETE FROM albums WHERE artist = 'The Beatles';
