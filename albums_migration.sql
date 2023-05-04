81d1a73e7f4c3198e5e5c76f76c1c948172a56e4
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-05-04 09:26:09 -0500

    completed 1-4

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,8 @@ CREATE TABLE albums (
     id int UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    release_date INT, sales FLOAT,
+    release_date INT,
+    sales FLOAT,
     genre VARCHAR(100),
     PRIMARY KEY (id)
 );
--- select_exercises.sql
+++ select_exercises.sql
@@ -2,3 +2,7 @@ USE codeup_test_db;
 
 SELECT name AS 'Names of all albums by Pink Floyd' FROM albums WHERE artist = 'Pink Floyd';
 SELECT release_date AS 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT genre AS 'The genre for Nevermind' FROM albums where NAME = 'Nevermind';
+SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT * FROM albums WHERE sales < 20.0;
+SELECT * FROM albums WHERE genre = 'Rock';
