13133b45abb027668ac646af90c5b84cd702ad5b
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-05-04 09:14:58 -0500

    db exercises

---
+++ select_exercises.sql
@@ -0,0 +1,4 @@
+USE codeup_test_db;
+
+SELECT name AS 'Names of all albums by Pink Floyd' FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date AS 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
