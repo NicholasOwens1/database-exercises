f3f4672f86376c2bef5d965fd6d717e6c8a2ce86
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-05-05 11:06:28 -0500

    completed order by exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,5 +1,5 @@
 USE employees;
-SELECT * from employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT first_name, last_name from employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name DESC;
 SELECT * from employees WHERE first_name LIKE 'E%';
 SELECT * from employees WHERE last_name LIKE '%q%';
 
@@ -7,7 +7,7 @@ SELECT * from employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR f
 
 SELECT * from employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
-SELECT * from employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+SELECT last_name, emp_no from employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' ORDER BY emp_no DESC;
 
 SELECT * from employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%qu%';
 
